85a7e8de27c4e28dbd21db8cea2719ddafbef14f
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-11-27 17:30:46 +0530

    Promise params

--- app/pages/users/[id]/page.tsx
+++ app/pages/users/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const UserProfile = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
+const UserProfile = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
   return <div>UserProfile {id}</div>;
 };
 
